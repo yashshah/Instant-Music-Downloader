156b3b0e96da9c09a6633c9ab9973a6e9377903c
Yash Shah <mail@yashshah.com>
2016-08-22 17:18:52 +0530

    Fix the SearchBox layout

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,16 +3,34 @@ import { default as React, Component } from 'react';
 export class SearchBox extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inputValue: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleSubmit(event) {
+    console.log("Inside the Submit");
+    event.preventDefault();
+  }
+  handleInputChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
   }
   render() {
     return (
-      <div className="col s12 m10 l8 offset-m1 offset-l2">
-        <div className="search">
-          <div className="search-wrapper card">
-            <form id="search-form">
-              <input id="search-input" type="text" placeholder="Enter a song name, lyrics, artists.. anything!" />
-            </form>
-          </div>
+      <div className="search">
+        <div className="search-wrapper card">
+          <form id="search-form" onSubmit={this.handleSubmit}>
+            <input
+              id="search-input"
+              type="text"
+              placeholder="Enter a song name, lyrics, artists.. anything!"
+              value={this.state.inputValue}
+              onChange={this.handleInputChange}
+              />
+          </form>
         </div>
       </div>
     );
