ad2ea01180ccd70d61dc896f5ae9f1b93bf199f0
Yash Shah <mail@yashshah.com>
2016-08-22 17:19:25 +0530

    Fetch youtube URL in MusicPlayer component

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,5 +1,8 @@
 import { default as React, Component } from 'react';
+import axios from 'axios';
+import config from '../config.js';
 
+let youtubeURL = 'https://www.googleapis.com/youtube/v3/search';
 
 export class MusicPlayer extends Component {
   constructor(props) {
@@ -14,6 +17,34 @@ export class MusicPlayer extends Component {
     };
   }
 
+  componentDidMount() {
+    let self = this;
+    config.q = this.state.query;
+    axios({
+      url: youtubeURL,
+      method: 'get',
+      params: config
+    })
+    .then(function (response) {
+      if (!response.data || !response.data.items || response.data.items.length === 0) {
+        console.log("no results while searching for: '" + config.q + "'.");
+      } else {
+        self.setPlayerTitle(response.data.items[0].snippet.title, response.data.items[0].id.videoId);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
+  setPlayerTitle(title, videoId) {
+    let downloadLink = `http://youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=${videoId}`;
+    let downloadButton = <a href={downloadLink} className='download-link icon-arrow-down'>Download</a>;
+    this.setState({
+      title: downloadButton + title
+    });
+  }
   render() {
     return (
       <div id="player">
