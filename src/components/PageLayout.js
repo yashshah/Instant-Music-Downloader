ff013693c3d5ec355ca7ecf1e586895394caca3d
Yash Shah <mail@yashshah.com>
2016-08-22 17:19:35 +0530

    Add MusicPlayer in PageLayout

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,5 +1,6 @@
 import { default as React, Component } from 'react';
 import {SearchBox} from './SearchBox';
+import {MusicPlayer} from './MusicPlayer';
 
 export class PageLayout extends Component {
   constructor(props) {
@@ -10,7 +11,12 @@ export class PageLayout extends Component {
       <div className="container container-table">
         <div className="row vertical-center-row">
           <h2 className="white-text center">Play any song instantly</h2>
-          <SearchBox />
+          <div className="col s12 m10 l8 offset-m1 offset-l2">
+            <SearchBox />
+          </div>
+          <div className="col s12 m10 l8 offset-m1 offset-l2">          
+            <MusicPlayer />
+          </div>            
         </div>
       </div>
     );
