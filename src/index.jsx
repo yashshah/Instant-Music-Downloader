858fb202b7dcc4b236f7cc0e5e268be1d5429e7e
Yash Shah <mail@yashshah.com>
2016-08-22 12:56:04 +0530

    Render PageLayout in Index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,17 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import { default as React, Component } from 'react';
 import { render } from 'react-dom';
-import {HomePage} from './components/HomePage'
-import {CounterButton} from './components/CounterButton'
+import {PageLayout} from './components/PageLayout';
+import './styles/index.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return(
-      <div>
-        <HomePage />
-        <CounterButton / >
+    return (
+      <div className="background">
+        <PageLayout />
       </div>
-    )
+    );
   }
 }
 
-render(<App/>, document.querySelector("#app"));
+render(<App />, document.querySelector("#app"));
