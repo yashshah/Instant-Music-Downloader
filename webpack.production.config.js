6f9c00ff8cb96fd405a755d51227c7f5ca54aa60
Yash Shah <mail@yashshah.com>
2016-08-22 12:53:31 +0530

    Include css directly in webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -8,7 +8,7 @@ var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 // local css modules
 loaders.push({
 	test: /[\/\\]src[\/\\].*\.css$/,
-	loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+	loader: ExtractTextPlugin.extract('style', 'css')
 });
 // global css files
 loaders.push({
