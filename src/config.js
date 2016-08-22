d11963d9756fea1d943a9482d20fb577ee7dc334
Yash Shah <mail@yashshah.com>
2016-08-22 17:15:31 +0530

    Add youtube config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  key: "AIzaSyAeRaUDeX63abVtQ1RkuiVbO9e21lKWimA",
+  type: "video",
+  part: "id,snippet",
+  fields: 'items/id/videoId,items/snippet/title',
+  maxResults: 1
+};
